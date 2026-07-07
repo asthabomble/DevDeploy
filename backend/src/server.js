d1c0c45504d808a0a000fe40f01275f71d59c84f
Astha Bomble <asthabomble@gmail.com>
2026-07-07 21:00:22 +0530

    Create User model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,21 @@
+const dotenv = require("dotenv");
+
+// Load .env file
+const result = dotenv.config();
+
+console.log("Dotenv Result:");
+console.log(result);
+
+console.log("PORT =", process.env.PORT);
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
+const connectDB = require("./config/db");
 const app = require("./app");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
